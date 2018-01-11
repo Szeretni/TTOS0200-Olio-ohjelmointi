77535b3f2270c97576202e73cf2061751f3727ad
Hannu Oksman <hannu@muaddibs.net>
2018-01-11 18:38:49 +0200

    First L02 assignments done

---
+++ L02/L02_assignment01/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Hannu Oksman
+Tee kahden sisäkkäisen for-silmukan avulla ohjelma, joka tulostaa seuraavanlaisen kuvion:
+    *
+    **
+    ***
+    ****
+    *****
+ 
+Käyttäjä antaa syötteenä tähtirivien lukumäärän. Yllä olevassa esimerkissä käyttäjä on syöttänyt luvun 5.
+Esimerkkitoiminta:
+    Give a number: 3 [Enter]
+    *
+    **
+    ***
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L02_assignment01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give a number: ");
+            int answer = int.Parse(Console.ReadLine());
+            for (int i = 0; i < answer;i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ L02/L02_assignment02/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Hannu Oksman
+Tee ohjelma, joka kysyy käyttäjältä 5 kokonaislukua. Luvut tulee sijoittaa taulukkoon. Ohjelman tulee tulostaa annetut luvut käänteisessä järjestyksessä.
+
+Esimerkkitoiminta:
+
+Give a number: 1 [Enter]
+    Give a number: 2 [Enter]
+    Give a number: 3 [Enter]
+    Give a number: 4 [Enter]
+    Give a number: 5 [Enter]
+    Number are 5,4,3,2,1
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L02_assignment02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
