ce6fe91fd3790eb458d425fdf1456c6ab4be99ad
Hannu Oksman <hannu@muaddibs.net>
2018-01-11 16:53:13 +0200

    first commit

---
+++ assignment02/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give a number > ");
+            int number = int.Parse(Console.ReadLine());
+            switch (number)
+            {
+                case 1: Console.WriteLine("Number is one");
+                    break;
+                case 2: Console.WriteLine("Number is two");
+                    break;
+                case 3: Console.WriteLine("Number is three");
+                    break;
+                default: Console.WriteLine("Joku muu luku");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
