a0fecbdaf1586a22694174685ec9e91f0d501ee9
Hannu Oksman <hannu@muaddibs.net>
2018-01-23 00:13:33 +0200

    formats improvements

--- L05/L05_assignment01/ItMath.cs
+++ L05/L05_assignment01/ItMath.cs
@@ -22,8 +22,77 @@ namespace L05_assignment01
             CultureInfo fi = new CultureInfo("fi-FI");
             string dateStr = "d";
             string montStr = "M";
-            string yeareStr = "y";
-
+            string yearStr = "yy";
+            int arraySize = (int)Math.Pow(2, 3);
+            string[] formats = new string[arraySize];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < arraySize;j++)
+                {
+                    switch(i)
+                    {
+                        case 0:
+                            {
+                                if (j < arraySize / 2)
+                                {
+                                    formats[j] = formats[j] + dateStr;
+                                }
+                                else
+                                {
+                                    for (int k = 0; k < 2; k++)
+                                    {
+                                        formats[j] = formats[j] + dateStr;
+                                    }
+                                }
+                                break;
+                            }
+                        case 1:
+                            {
+                                if (j < arraySize / 2)
+                                {
+                                    formats[j] = formats[j] + "." + montStr;
+                                }
+                                else
+                                {
+                                    for (int k = 0; k < 2; k++)
+                                    {
+                                        if (k == 0) formats[j] = formats[j] + ".";
+                                        formats[j] = formats[j] + montStr;
+                                    }
+                                }
+                                break;
+                            }
+                        case 2:
+                            {
+                                if (j < arraySize / 2)
+                                {
+                                    formats[j] = formats[j] + "." + yearStr;
+                                }
+                                else
+                                {
+                                    for (int k = 0; k < 2; k++)
+                                    {
+                                        if (k == 0) formats[j] = formats[j] + ".";
+                                        formats[j] = formats[j] + yearStr;
+                                    }
+                                }
+                                break;
+                            }
+                    }
+                }
+            }
+            /*int arrayIndex = 0;
+            for (int i = 1; i < 3;i++ )
+            {
+                for (int j = 1; j < 3; j++)
+                {
+                    for (int k = 1; k < 3; k++)
+                    {
+                        formats[arrayIndex] = dateStr*i +
+                        arrayIndex++;
+                    }
+                }
+            }*/
             //string[] formats = { "d.M.yy", "d.MM.yy", "dd.M.yyyy", "d.M.yyyy", "dd.M.yy", "dd.MM.yy", "dd.MM.yy", "dd.MM.yyyy" };          
             DateTime boolDate = DateTime.Today;
             //input = input + " 0.0.0";
