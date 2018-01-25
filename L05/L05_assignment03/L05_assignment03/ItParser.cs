262a6a7c998afac3b7994165ab82115dbbef12dc
Hannu Oksman <hannu@muaddibs.net>
2018-01-25 18:49:25 +0200

    L05_03 in progress

---
+++ L05/L05_assignment03/L05_assignment03/ItParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L05_assignment03
+{
+    class ItParser
+    {
+        public static int Count(string input)
+        {
+            //1 because ie. 2 integers are separated by 1 dot
+            int countDots = 1;
+            foreach (char c in input)
+            {
+                if (c == ',') countDots++;
+            }
+            return countDots;
+        }
+
+        public static int Sum(string input)
+        {
+            int sum = 0;
+            
+            return 0;
+        }
+
+        public static int Average(string input)
+        {
+            return 0;
+        }
+
+        //private int count = 0;
+        //private int sum = 0;
+        //private int average = 0;
+    }
+}
---
+++ L05/L05_assignment03/L05_assignment03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L05_assignment03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Enter integers and separate them by ','. Ie. 50,60,70,80,90.");
+                Console.Write("Your input: ");
+                string inputString = Console.ReadLine();
+                int output = ItParser.Count(inputString);
+                Console.WriteLine(output);
+            }
+            
+        }
+    }
+}
