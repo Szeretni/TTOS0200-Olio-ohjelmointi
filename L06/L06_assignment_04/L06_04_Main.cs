6c5648012500fd05a096c4ee6215f4231a8ba498
Hannu Oksman <hannu@muaddibs.net>
2018-01-30 18:20:04 +0200

    L06_04 init

---
+++ L06/L06_assignment_04/L06_04_Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L06_assignment_04
+{
+    class L06_04_Main
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
