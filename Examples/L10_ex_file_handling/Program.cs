252331c4f6e559799fb3194566babe8ae440ec5f
Hannu Oksman <hannu@muaddibs.net>
2018-02-08 18:32:35 +0200

    L10 examples

---
+++ Examples/L10_ex_file_handling/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L10_ex_file_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ L10/L10_assignment_01/L10_01_Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO; //streams
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class NamesToFile
+    {
+        static void Test()
+        {
+            try
+            {
+                string line;
+                string myfile = @"D:\l2912\testi.txt"; // @ kertoo, että \ ei ole ohjausmerkki, ilman D:\\l29...
+                //luodaan tiedosto
+                using (StreamWriter sw = new StreamWriter(myfile))
+                {
+                    //kysytään käyttäjältä nimet ja kirjoitetaan nimet tiedostoon
+                    do
+                    {
+                        Console.WriteLine("Anna henkilön nimi (tyhjä lopettaa)");
+                        line = Console.ReadLine();
+                        if (line.Length > 0)
+                        {
+                            sw.WriteLine(line);
+                        }
+                    } while (line.Length > 0);
+                    sw.Close();
+                }
+                //avataan tiedosto lukua varten
+                if (File.Exists(myfile))
+                {
+                    //näytetään nimet
+                    string txt = File.ReadAllText(myfile);
+                    Console.WriteLine("Tiedostossa {0} on nimet: {1}", myfile, txt);
+                }
+                else
+                {
+                    Console.WriteLine("Tiedostoa {0} ei löydy.",myfile);
+                }
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Test();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+        }
+    }
+}
