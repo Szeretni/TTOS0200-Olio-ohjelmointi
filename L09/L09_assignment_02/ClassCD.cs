b11ceb7373c09e99450f93e37633663572aee50a
Hannu Oksman <hannu@muaddibs.net>
2018-02-11 16:47:16 +0200

    L09 assignment 02 done

---
+++ L09/L09_assignment_02/ClassCD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L03_assignment07
+{
+    class ClassCD
+    {
+        //creates unlimited songlist
+        public List<ClassSongs> SongList  = new List<ClassSongs>();
+        //public ClassSongs[] SongList = new ClassSongs[Int16.MaxValue];
+        public string Artist { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ L09/L09_assignment_02/ClassSongs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L03_assignment07
+{
+    class ClassSongs
+    {
+        //read-only
+        public DateTime Duration
+        {
+            get
+            {
+                return duration;
+            }
+        }
+        public string Name { get; set; }
+
+        //adds duration
+        public void DurationMethod(int minutes, int seconds)
+        {
+            duration = duration.AddMinutes(minutes);
+            duration = duration.AddSeconds(seconds);
+        }
+
+        private DateTime duration = new DateTime(2018, 1, 1, 0, 0, 0);
+    }
+}
