26a9b1e8734737ac3a202d70f9465a5e4501e3cd
Hannu Oksman <hannu@muaddibs.net>
2018-02-11 21:58:47 +0200

    L09 assignment 03 done

---
+++ L09/L09_assignment_03/Card.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L09_assignment_03
+{
+    class Card
+    {
+        //constructor
+        public Card(int Country, int Rank)
+        {
+            this.Country = Country;
+            this.Rank = Rank;
+        }
+
+        //properties
+        public int Country { get; set; }
+        public int Rank { get; set; }
+        private string diamond = "Diamond";
+        private string club = "Club";
+        private string spade = "Spade";
+        private string heart = "Heart";
+
+        //methods
+        public override string ToString()
+        {
+            string country;
+            if (Country == 1)
+            {
+                country = diamond;
+            }
+            else if (Country == 2)
+            {
+                country = club;
+            }
+            else if (Country == 3)
+            {
+                country = spade;
+            }
+            else
+            {
+                country = heart;
+            }
+            return country + "#" + Rank;
+
+            //not so good
+            /*
+            if (Country == 1)
+            {
+                return diamond + "#" + Rank;
+            }
+            else if (Country == 2)
+            {
+                return club + "#" + Rank;
+            }
+            else if (Country == 3)
+            {
+                return spade + "#" + Rank;
+            }
+            else
+            {
+                return heart + "#" + Rank;
+            }
+            */
+        }
+    }
+}
---
+++ L09/L09_assignment_03/L09_03_Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L09_assignment_03
+{
+    class L09_03_Main
+    {
+        static void Main(string[] args)
+        {
+            //collection
+            //List<Card> cardSet = new List<Card>();
+            Dictionary<int, Card> cardSet = new Dictionary<int, Card>();
+
+            //card generation
+            int cardID = 1;
+            for (int i = 1; i<=4;i++)
+            {
+                for (int j = 1; j<=13;j++)
+                {
+                    cardSet.Add(cardID,new Card(i, j));
+                    cardID++;
+                }
+            }
+
+            //card listing
+            foreach (KeyValuePair<int,Card> kvp in cardSet)
+            {
+                Console.WriteLine("{0} card is {1}", kvp.Key, kvp.Value.ToString());
+            }
+        }
+    }
+}
